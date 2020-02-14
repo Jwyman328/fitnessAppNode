0424e8111732cfc5727cad454000f3294b69a5f7
Joseph Wyman <jwyman@Josephs-MacBook-Pro.local>
2020-02-14 09:21:22 -0500

    Add get activity input by id and user

--- src/routers/activityInput.js
+++ src/routers/activityInput.js
@@ -14,4 +14,33 @@ activityInputRouter.post('/activityInput/',async(req,res) => {
     }
 })
 
+// get individual activity input 
+activityInputRouter.get('/activityInput/:id/', async(req, res) => {
+    try{
+        console.log(req)
+        const activityInput = await ActivityInput.findById(req.params.id);
+        if (activityInput){
+            res.send(activityInput)
+        }else{
+            res.send('activity input requested not found')
+        }
+    }catch(error){
+        console.log(error)
+        res.status(400)
+        res.send('error on fetching activity input')
+    }
+})
+
+// get all of a user's activity inputs
+activityInputRouter.get('/allActivityInputs/', async(req, res) => {
+    try{
+        const allActivityInputs = await ActivityInput.find(req.body);
+        // return value even if empty 
+        res.send(allActivityInputs)
+    }catch(error){
+        res.status(404)
+        res.send("error fetch all use activity inputs")
+    }
+})
+
 module.exports = activityInputRouter;
