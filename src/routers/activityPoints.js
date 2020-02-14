ceb0512311057bf065dcd149a8b38fd72da0899f
Joseph Wyman <jwyman@Josephs-MacBook-Pro.local>
2020-02-14 08:58:29 -0500

    Get all activity points end point

--- src/routers/activityPoints.js
+++ src/routers/activityPoints.js
@@ -5,7 +5,7 @@ const activityPointRouter = new express.Router()
 
 activityPointRouter.post('/activityPoint/', async(req,res) => {
     try{
-        const newActivityPoint = new ActivityPoint(req.body)
+        const newActivityPoint = new ActivityPoint(req.body);
         const savedActivityPoint = await newActivityPoint.save();
         res.send(`activity point created ${savedActivityPoint}`);
     }catch(error){
@@ -14,6 +14,40 @@ activityPointRouter.post('/activityPoint/', async(req,res) => {
     }
 })
 
+// find specific activity point by id
+activityPointRouter.get('/activityPoint/:id/', async(req,res)=>{
+    try{
+        const specificActivityPoint = await ActivityPoint.findById(req.params.id);
+        //if activity point found.
+        if (specificActivityPoint){
+            res.send(specificActivityPoint);
+        }else{
+            // if empty 
+            res.send('activity point not found')
+        }
+    }catch(error){
+        res.status(400)
+        res.send('Error fetching activity point')
+    }
+})
+
+//find all activity points for user 
+activityPointRouter.get('/allActivityPoints/', async(req,res)=>{
+    try{
+        console.log(req);
+        const allUserActivityPoints = await ActivityPoint.find(req.body);
+        //if activity point found.
+        if (allUserActivityPoints){
+            res.send(allUserActivityPoints);
+        }else{
+            // if empty 
+            res.send('activity points not found')
+        }
+    }catch(error){
+        res.status(400)
+        res.send('Error fetching all activity point')
+    }
+})
 
 
 module.exports = activityPointRouter
