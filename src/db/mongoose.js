d5ae5e876537681e14244f9f0ce2e93bd00c7cfd
Joseph Wyman <jwyman@Josephs-MacBook-Pro.local>
2020-02-19 13:37:03 -0500

    correct mongodb code

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 //mongodb://<dbuser>:<dbpassword>@ds031203.mlab.com:31203/heroku_tpgqpjr3
 //'mongodb://127.0.0.1:27017/fitnessNode'
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds031203.mlab.com:31203/heroku_tpgqpjr3', {
+
+mongoose.connect('mongodb://heroku_tpgqpjr3:ad457i7ia9sqe8nshqnd2ob24p@ds031203.mlab.com:31203/heroku_tpgqpjr3', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true 
