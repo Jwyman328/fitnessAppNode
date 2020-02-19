773f46f8707ebcaf878180bb498e846a09c605ce
Joseph Wyman <jwyman@Josephs-MacBook-Pro.local>
2020-02-19 13:24:48 -0500

    Add mlab connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/fitnessNode', {
+//mongodb://<dbuser>:<dbpassword>@ds031203.mlab.com:31203/heroku_tpgqpjr3
+//'mongodb://127.0.0.1:27017/fitnessNode'
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds031203.mlab.com:31203/heroku_tpgqpjr3', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true 
