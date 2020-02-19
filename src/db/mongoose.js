86e9899e4811f251600c98b02e0518b81581d25d
Joseph Wyman <jwyman@Josephs-MacBook-Pro.local>
2020-02-19 15:01:55 -0500

    add heroku config var

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 //mongodb://<dbuser>:<dbpassword>@ds031203.mlab.com:31203/heroku_tpgqpjr3
 //'mongodb://127.0.0.1:27017/fitnessNode'
 
-mongoose.connect('mongodb://heroku_tpgqpjr3:ad457i7ia9sqe8nshqnd2ob24p@ds031203.mlab.com:31203/heroku_tpgqpjr3', {
+const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fitnessNode';
+
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true 
