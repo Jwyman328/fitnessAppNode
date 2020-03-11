fcd2e9a3407b3a6fce0ce7a7cbca9ac0be71b274
Joseph Wyman <jwyman@Josephs-MacBook-Pro.local>
2020-03-11 11:01:50 -0400

    Add redis env variable

--- src/db/redisClient.js
+++ src/db/redisClient.js
@@ -1,7 +1,10 @@
 
+const redisUri = process.env.REDIS_URL || "redis://redis:6379";
+
+
 const redis = require('redis')
 const bluebird = require('bluebird') 
 bluebird.promisifyAll(redis)
-const redisClient = redis.createClient("redis://redis:6379")
+const redisClient = redis.createClient(redisUri)
 
 module.exports = {redisClient}
