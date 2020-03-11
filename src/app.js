340e6fd6846ca992d95d9be2a763ac85950a38a1
Joseph Wyman <jwyman@Josephs-MacBook-Pro.local>
2020-03-11 15:19:51 -0400

    Add gzip with compression

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 
 require('./db/mongoose')
+var compression = require('compression')
+
 const helmet = require('helmet')
 
 const express = require('express')
@@ -14,7 +16,7 @@ var cors = require('cors')
 const app = express();
 app.use(helmet())
 app.use(cors())
-
+app.use(compression())
 app.use(express.json());
 app.use(activityInputRouter);
 app.use(userRouter);
