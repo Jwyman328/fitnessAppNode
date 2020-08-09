093285a8a776136005f7266828bf7e452b7be61f
Joseph Wyman <jwymandev@gmail.com>
2020-08-09 14:40:45 -0400

    missing ActivityPoint

--- src/routers/totalPointGoal.js
+++ src/routers/totalPointGoal.js
@@ -2,6 +2,7 @@ const express = require("express");
 const auth = require("../middleware/auth");
 const TotalPointGoal = require("../models/totalPointGoal");
 const totalPointGoalRouter = new express.Router();
+const ActivityPoint = require("../models/activityPoints");
 
 /**
  * Return sum of total points for a range of dates.
